d26f2ac164ab55ab1d3b7bd1959a78a46f1cf9a0
Adam Curtis <acurtis1286@gmail.com>
2020-03-20 13:30:48 -0400

    Made a template layout

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <div class="welcome-jumbo">
+        <div class="status">OLD</div>
+        <h1>About Page</h1>
+    </div>
+@endsection
---
+++ resources/views/pages/waitlist.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <div class="welcome-jumbo">
+        <div class="status">OLD</div>
+        <h1>Waitlist Page</h1>
+    </div>
+@endsection
