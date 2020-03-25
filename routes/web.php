f6d74e8aee54ce546f91c67da0ac1b00630c96ce
Adam Curtis <acurtis1286@gmail.com>
2020-03-25 16:34:54 -0400

    Fixed register page for authentication

--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,10 @@ Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
 
 // Admin Authentication
 Route::get('/admin/register', function () {
-    return view('admin/register');
+    return view('/admin/register');
 });
 Route::get('/admin/login', function () {
-    return view('admin/login');
+    return view('/admin/login');
 });
 
 
